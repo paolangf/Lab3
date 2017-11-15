528a3c6868f1f3af39112a8afcbc4af392ea02d3
4015e8 <4015e8@guayaba.ece.uprm.edu>
2017-11-15 11:06:23 -0400

    Added MyPanelClass, Called MyPanelClass methods in Lab3Exercise Class. In MyPanelClass changed color and added border

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,14 @@
 import javax.swing.JFrame;
 
+
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Paola Guadalupe");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(900, 460);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
---
+++ Lab3/src/MyPanelClass.java
@@ -0,0 +1,33 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.JPanel;
+ 
+public class MyPanelClass extends JPanel {
+            /**
+	 * 
+	 */
+	private static final long serialVersionUID = 7057541440811488699L;
+
+			public void paintComponent(Graphics g) {
+                        super.paintComponent(g);
+ 
+                        //Compute interior coordinates
+                        Insets myInsets = this.getInsets();
+                        int x1 = myInsets.left;
+                        int y1 = myInsets.top;
+                        int x2 = getWidth() - myInsets.right - 1;
+                        int y2 = getHeight() - myInsets.bottom - 1;
+                        int width = x2 - x1;
+                        int height = y2 - y1;
+ 
+                        //Paint the background
+                        g.setColor(Color.RED);
+                        g.fillRect(x1, y1, width + 1, height + 1);
+                        //Draw a border
+                        g.setColor(Color.YELLOW);
+                        g.drawRect(x1, y1, width, height);
+                        g.setColor(Color.WHITE);
+                        g.drawRect(x1 + 5, y1 + 5, width - 10, height - 10);
+            }
+}

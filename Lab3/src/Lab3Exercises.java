f349da87d1b0c0a6a51da229707e84a062a9d8a3
4015e8 <4015e8@guayaba.ece.uprm.edu>
2017-11-15 10:41:40 -0400

    Change in location y-axis.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Paola Guadalupe");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 450);
+		myFrame.setLocation(900, 460);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
